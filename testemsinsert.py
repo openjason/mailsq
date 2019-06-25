d31793131e0c202cadae74fe42f496d8512408d2
openjason <openjc@outlook.com>
2019-06-25 15:27:03 +0800

    0626

--- testemsinsert.py
+++ testemsinsert.py
@@ -5,7 +5,7 @@ datestr = time.strftime('%Y%m%d', time.localtime(time.time()))
 timestr = time.strftime('%H%M%S', time.localtime(time.time()))
 
 def InvokeWebservice(snumber,mailnum):
-    texturl = 'http://1.1.1.64:8008/rfromems?wsdl'
+    texturl = 'http://112.91.148.118:10000/post/EMS'
 #    texturl='http://localhost:8008/Queryemsmail?wsdl'
 # rfems: Recieve from ems
 
@@ -16,16 +16,16 @@ def InvokeWebservice(snumber,mailnum):
     postcontent='''<?xml version="1.0" encoding="utf-8"?>
 <listexpressmail>
 <expressmail>
-<serialnumber> %(snumber)s </serialnumber>
-<mailnum> LK%(mailnum)sCN </mailnum>
-<procdate>  %(datestr)s </procdate>
-<proctime>%(timestr)s  </proctime>
-<orgfullname> 所在地名称 </orgfullname>
-<action> 00 </action>
-<description> 描述信息 </description>
-<effect> 1</effect>
-<properdelivery> 22 </properdelivery>
-<notproperdelivery> 100 </notproperdelivery>
+<serialnumber>%(snumber)s</serialnumber>
+<mailnum>%(mailnum)s</mailnum>
+<procdate>%(datestr)s</procdate>
+<proctime>%(timestr)s</proctime>
+<orgfullname>所在地名称</orgfullname>
+<action>00</action>
+<description>描述信息</description>
+<effect>1</effect>
+<properdelivery>22</properdelivery>
+<notproperdelivery>100</notproperdelivery>
 </expressmail>
 </listexpressmail>
 ''' % {'snumber': snumber, 'mailnum': mailnum,'datestr': datestr,'timestr': timestr}
@@ -35,10 +35,10 @@ def InvokeWebservice(snumber,mailnum):
     respRaw = urllib.request.urlopen(req)
     print(respRaw.read().decode('utf-8'))
 
-mailnum = 142005100
+mailnum = 1175642270178
 
 if __name__ == '__main__':
-    for i in range(4257,4259):
+    for i in range(20190617084004,20190617084006):
         mailnum = mailnum +1
         InvokeWebservice(str(i),str(mailnum))
         print (i)
